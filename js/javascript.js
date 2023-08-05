e61fb09a1cd4dfac1fc92572380f07701fecbaa6
RuduBear <134881527+rudubear@users.noreply.github.com>
2023-08-05 17:01:55 -0400

    refactor to use dom with new divs over console log

--- js/javascript.js
+++ js/javascript.js
@@ -1,4 +1,103 @@
-console.log("Welcome to the rock papers scissors game");
+let gameOn = true;
+let winners = [];
+let playerWinsCounter = 0;
+let computerWinsCounter = 0;
+
+const log = document.getElementById("log");
+log.style.border = "solid 2px black";
+
+const logEntry = document.createElement('div');
+logEntry.style.border = "solid 2px grey";
+logEntry.classList.add('logEntry');
+log.appendChild(logEntry);
+
+const resetButtons = document.getElementsByClassName("resetButtons");
+const resetButton = document.getElementById("resetGameBtn");
+const playButtons = document.getElementsByClassName("playButtons");
+
+resetButtons[0].style.display = "none";
+resetButton.addEventListener("click",resetGame)
+playButtons[0].style.display = "block";
+
+const btns = document.querySelectorAll(".makePlay");
+
+btns.forEach((btn) => {btn.addEventListener('click', function(e) {
+        const logEntry = document.createElement('p');
+        logEntry.style.border = "solid 2px grey";
+        logEntry.classList.add('logEntry')
+        logEntry.textContent = "";
+    
+        const playerChoice = (e.target.getAttribute("value"));
+        const computerChoice = getComputerChoice();
+
+        const result = playRockPaperScissors(playerChoice,computerChoice);
+        winners.push(result[0]);
+
+        if(result[0] === "player") {
+            updateRounds("playerRoundsWon");
+        } else if (result[0] === "computer") {
+            updateRounds("computerRoundsWon");
+        }
+
+        playerWinsCounter = winners.filter(playerWinsCount).length;
+        computerWinsCounter = winners.filter(computerWinsCount).length;
+        const logEntryPlayerResults = "Player has won " + playerWinsCounter + " times.";
+        const logEntryComputerResults = "Computer has won " + computerWinsCounter + " times.";
+
+        logEntry.textContent += "Player chose " + playerChoice + `, Computer chose ` + computerChoice + ".";
+        logEntry.textContent += "\n" + result[1];
+        
+        if (playerWinsCounter >= 5) {
+            logEntry.textContent += "Player has won this series!";
+            
+            winners = [];
+            playerWinsCounter = 0;
+            computerWinsCounter = 0;
+            updateScoreBoard("playerVictoriesCount");
+            seriesOver();
+        } else if (computerWinsCounter >=5) {
+            logEntry.textContent += "Computer has won this series!";
+            winners = [];
+            playerWinsCounter = 0;
+            computerWinsCounter = 0;
+            updateScoreBoard("computerVictoriesCount");
+            seriesOver();
+        }
+        log.appendChild(logEntry);
+    })
+
+});
+
+function updateScoreBoard(seriesWinnerCount) {
+    const victor = document.getElementById(seriesWinnerCount);
+    victor.textContent = Number(victor.textContent) + 1;
+}
+
+function seriesOver(){
+    playButtons[0].style.display = "none";
+    resetButtons[0].style.display = "block";
+}
+
+function resetRounds(){
+    const playerRoundsWon = document.getElementById("playerRoundsWon");
+    const computerRoundsWon = document.getElementById("computerRoundsWon");
+
+    playerRoundsWon.textContent = 0;
+    computerRoundsWon.textContent = 0;
+}
+
+function resetGame(){
+    playButtons[0].style.display = "block";
+    resetButtons[0].style.display = "none";
+
+    log.textContent = "Starting a new series";
+    resetRounds();
+}
+
+function updateRounds(roundWinner){
+    const roundsWon = document.getElementById(roundWinner);
+    roundsWon.textContent = Number(roundsWon.textContent)+1;
+}
 
 //game();
 
@@ -18,14 +117,13 @@ function getComputerChoice() {
             console.log("How did we get here?");
             break;
     }
-    console.log("Computer chose " + computerChoice);
     return computerChoice;
 }
 
 function getRandomInt(max = 3) {
     return Math.floor(Math.random() * max );
 }
-
+/*
 function getPlayerChoice(){
     let playerChoice;
     while(!(["rock", "paper", "scissors", null].includes(playerChoice)) ) {
@@ -36,6 +134,7 @@ function getPlayerChoice(){
     console.log("Player chose " + playerChoice);
     return playerChoice;    
 }
+*/
 
 function playRockPaperScissors(playerSelection, computerselection){
     let winner;
@@ -101,6 +200,7 @@ function computerWinsCount(winner){
     return winner === "computer";
 }
 
+/*
 function game(){
     let gameOn = true;
     let winners = [];
@@ -137,4 +237,4 @@ function game(){
             gameOn = false;
         }
     }    
-}
+}*/
